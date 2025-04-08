92c2be544b4f55db7bd08ffe5865c585b7fc4a2a
Martin Kirov <skylinecho@abv.bg>
2025-04-08 21:20:01 +0300

    Adding VowelSum task

---
+++ 4-VowelSum/VowelSum.cs
@@ -0,0 +1,37 @@
+﻿namespace _4_VowelSum
+{
+    internal class VowelSum
+    {
+        static void Main(string[] args)
+        {
+            int countOfCharacters = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < countOfCharacters; i++)
+            {
+                char currentChar = char.Parse(Console.ReadLine());
+                if (currentChar == 'a')
+                {
+                    sum += 1;
+                }
+                else if (currentChar == 'e')
+                {
+                    sum += 2;
+                }
+                else if (currentChar == 'i')
+                {
+                    sum += 3;
+                }
+                else if (currentChar == 'o')
+                {
+                    sum += 4;
+                }
+                else if (currentChar == 'u')
+                {
+                    sum += 5;
+                }                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
