0407d6ac19f431065b76d230ee87e920390d409c
Martin Kirov <skylinecho@abv.bg>
2025-04-08 21:19:39 +0300

    Adding BiggestNumber task

---
+++ 3-BiggestNumber/BiggestNumber.cs
@@ -0,0 +1,21 @@
+﻿namespace _3_BiggestNumber
+{
+    internal class BiggestNumber
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int biggestNumber = int.MinValue;
+
+            for (int i = 1; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > biggestNumber)
+                {
+                    biggestNumber = number;
+                }
+            }
+            Console.WriteLine(biggestNumber);
+        }
+    }
+}
