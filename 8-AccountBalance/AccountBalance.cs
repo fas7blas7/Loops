7cc37c8480cfaf79c63a6cd4ef4ee730856ed175
Martin Kirov <skylinecho@abv.bg>
2025-04-10 21:02:21 +0300

    Adding AccountBalance task

---
+++ 8-AccountBalance/AccountBalance.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.AccountBalance
+{
+    internal class AccountBalance
+    {
+        static void Main(string[] args)
+        {
+            double balance = 0;
+            string line; //                                
+            while ((line = Console.ReadLine()) != "End")            
+            {                
+                double moneyAmount = double.Parse(line);
+                balance += moneyAmount;
+                if (moneyAmount >= 0)
+                {
+                    Console.WriteLine($"Increase: {moneyAmount:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"Decrease: {Math.Abs(moneyAmount):F2}");
+                }
+            }
+
+            Console.WriteLine($"Balance: {balance:F2}");
+        }
+    }
+}
